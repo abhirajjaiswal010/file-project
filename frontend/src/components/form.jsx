b5c7a9acb197a4abaf8b7e512341b6d074f94546
Abhiraj Jaiswal <160706531+abhirajjaiswal010@users.noreply.github.com>
2025-06-27 23:41:26 +0530

    frontend changes

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -220,7 +220,7 @@ export const Form = ({ formData, onInputChange, onSubmit }) => {
           {/* Submit */}
           <button
             type="submit"
-            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-300"
+            className="rounded-2xl border-2 border-dashed border-black bg-white px-6 py-3 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none"
           >
             Submit Order
           </button>
