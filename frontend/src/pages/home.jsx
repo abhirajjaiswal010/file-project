7050ae2cbfdfa902933f9092f0f255853286445c
Abhiraj Jaiswal <160706531+abhirajjaiswal010@users.noreply.github.com>
2025-06-28 00:15:17 +0530

    frontend changes

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -34,8 +34,13 @@ export const Home = () => {
           deserunt culpa inventore! Nam, repellendus.
         </p>
 
-        <button className="   bg-gradient-to-r from-blue-600 to-blue-500 text-white px-6 py-3 rounded-lg shadow-md hover:shadow-xl hover:from-blue-700 hover:to-blue-600 transition-all duration-300 ease-in-out"  onClick={() => navigate('/order')}>
-          🛒 Order Your Item
+        <button className="  rounded-2xl border-2 border-dashed border-black bg-white px-6 py-2 font-semibold uppercase text-black transition-all duration-300 hover:translate-x-[-4px] hover:translate-y-[-4px] hover:rounded-md hover:shadow-[4px_4px_0px_black] active:translate-x-[0px] active:translate-y-[0px] active:rounded-2xl active:shadow-none block mx-auto"  onClick={() => navigate('/order')}>
+          <div className="flex flex-row justify-center items-center gap-5
+          ">
+            <img src="/cart.gif" alt="" className="h-8"/>
+              Order Your Item
+          </div>
+         
         </button>
       </div>
     </div>
